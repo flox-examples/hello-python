3535d60b79117770c0befb2cabff0bd6e3cabb91
Matthew Kenigsberg <matthewkenigsberg@gmail.com>
2022-09-08 09:39:07 -0600

    fix: use latest changes in floxpkgs template
    Helps https://github.com/flox/flox/issues/192

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,12 +1,6 @@
 {python3Packages}:
-python3Packages.buildPythonApplication {
-  pname = "hello";
+python3Packages.buildPythonPackage {
+  pname = "hello-python";
   version = "0.0.0";
   src = ../.;
-  PIP_DISABLE_PIP_VERSION_CHECK=1;
-
-  propagatedBuildInputs = with python3Packages; [
-    requests
-    # Add Python modules needed by your package here
-  ];
 }
